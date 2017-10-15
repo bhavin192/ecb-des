9316789f6749fd452e641d59d9654fb73002d5ed
Bhavin Gandhi <bhavin7392@gmail.com>
2017-10-15 23:10:49 +0530

    Add initial code This includes a lot of TODOs to complete, for now it takes second argument and divides it into arrays of 8 characters each, so that we can convert it to 64 character binary array. Also the given argument is copied into character array of size 256. It helps to create fixed number of arrays from it, which is 32 *will be changed later*
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

---
+++ ecb-des.c
@@ -0,0 +1,41 @@
+#define _GNU_SOURCE  
+#include <crypt.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char key[64];
+    char input[256];
+    char divided_input[32][8];
+    char bin_input[32][64];
+    struct crypt_data data;
+    data.initialized = 0;
+    
+    //TODO: handle all command line arguments properly
+    strcpy(input, argv[2]);
+    
+    //TODO: parallelize this 
+    for(int i = 0; i < 32; i++)
+        for(int j = 0; j < 8; j++)
+            divided_input[i][j] = input[i * 8 + j];
+    
+    //TODO: convert all arrays to bit form 
+    
+    //TODO: generate readable random key 
+    /*
+    for(int i = 0; i < 64; i++)
+        key[i] = rand() & 1;
+    */    
+    
+    //TODO: encrypt all blocks 
+    //setkey_r(key, &data);
+    
+    //TODO: convert the bit arrays to readable form 
+    
+    return 0;
+}
+
+//http://www.gnu.org/software/libc/manual/html_node/crypt.html#crypt
+//http://man7.org/linux/man-pages/man3/encrypt.3.html
+//https://stackoverflow.com/questions/4892579/how-to-convert-a-char-to-binary
